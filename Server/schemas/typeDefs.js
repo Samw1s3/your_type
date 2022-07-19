5954f2e8f0b6027cdd1883f1da024ea08feb6626
Sam Knopper <samanthaknopper@hotmail.com>
2022-07-19 20:35:04 +0800

    typeDefs finished, page files created

---
+++ Server/schemas/typeDefs.js
@@ -0,0 +1,47 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    meetupCount: Int
+    savedMeetups: [Meetups]
+  }
+
+  type Auth {
+    token: String!
+    user: User!
+  }
+
+  type Meetup {
+    meetupId: String!
+    name: String!
+    hairColor: Int
+    height: Int
+    physicalAttributes: Int
+    conversation: Int
+    laughs: Int 
+    butterflies: Int
+    eyeContact: Int
+    smile: Int 
+    generalVibe: String      
+  }
+
+  type Query {
+    meetups: [Meetup]!
+    (thoughtId: ID!): Thought
+  }
+
+  type Mutation {
+    
+    login(email: String!, password: String!):Auth
+    saveMeetup(meetupId: String, name: String, hairColor: Int,height: Int, physicalAttributes: Int, conversation: Int, laughs: Int, butterflies: Int, eyeContact: Int, smile: Int, generalVibe: String ):User
+    removeMeetup(meetupId: String!): User
+    addUser(username: String!, email: String!, password: String!):Auth
+
+  }
+`;
+
+
+module.exports = typeDefs;
